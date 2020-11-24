a093d47de5a12b954292e501fa51b948c8ec0fc7
currysun <s15639107@stu.edu.tw>
2020-11-24 15:35:19 +0800

    requirements has fixed

---
+++ obj/Any CPU/Release/Package/PackageTmp/huskyPy/views.py
@@ -0,0 +1,50 @@
+"""
+Routes and views for the flask application.
+"""
+from datetime import datetime
+from huskyPy import app
+from flask import render_template
+#
+#from quart import render_template
+from huskyPy.crawl.crawl_web import *
+#
+import time
+
+@app.route('/')
+@app.route('/home')
+def home():
+    
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/crawl')
+def crawl():
+    #cwb_crawl()
+    result_obj = cwb_crawl()
+    #print(result_obj)
+
+    return "getURL=>" + result_obj
+    pass
