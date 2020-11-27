aa8389aea0aaddb39b91e1bab70af1a51b59b3af
currysun <s15639107@stu.edu.tw>
2020-11-27 17:16:25 +0800

    fix Procfile 6

--- runserver.py
+++ runserver.py
@@ -7,7 +7,7 @@ from huskyPy import app
 
 if __name__ == '__main__':
     #HOST = environ.get('SERVER_HOST', 'localhost')
-    HOST = environ.get('SERVER_HOST', '0.0.0.0')
+    HOST = environ.get('SERVER_HOST', '127.0.0.1')
 
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
