8bedb0c5c4474731e21331b4c5d1217f1e801891
currysunyang <s15639107@stu.edu.tw>
2020-11-29 14:28:13 +0800

    ready to develop line api

--- huskyPy/lineapi/linebot_reply.py
+++ huskyPy/lineapi/linebot_reply.py
@@ -7,6 +7,6 @@ from huskyPy.crawl.crawl_web import *
 line_bot_api = LineBotApi("a8VhBE2YuovRR+T+qqCKW0r0NrRHxlM4XHapAA/N3RD4VHqXlbzVl5zJhBUJZqFI7AZtV+l6wBYoNuhY2+C2HiJ5gfKA6J/TX/YFrWUrlqD+2CzRnlrj5tSuKxhmOGN4JvjEcSKaONgaJCS8WOt1dQdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("1501871c42abe646feb988e8cf56011e")
 
-@app.route("/callback", methods=['POST'])
-def callback():
+@app.route("/api_huskypy", methods=['POST'])
+def api_huskypy():
 	return "OK"
